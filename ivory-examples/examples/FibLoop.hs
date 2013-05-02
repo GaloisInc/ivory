c45a86a9964a963d533c9c246048e1374d5242c3
Pat Hickey <pat@galois.com>
2013-05-01 17:19:41 -0700

    ivory-examples: better fib example showing different styles

--- ivory-examples/examples/FibLoop.hs
+++ ivory-examples/examples/FibLoop.hs
@@ -1,3 +1,6 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 
@@ -8,34 +11,64 @@ import Ivory.Language
 
 import Control.Applicative ((<$>),(<*>))
 
+-- Recursive implementation of fib
+fib_rec :: Def ('[Uint32] :-> Uint64)
+fib_rec  = proc "fib_rec" (\n -> body (ret =<< call fib_rec_aux 0 1 n))
 
-fib :: Def ('[Uint32] :-> Uint64)
-fib  = proc "fib" (\n -> body (ret =<< call fib_aux 0 1 n))
-
-fib_aux :: Def ('[Uint32,Uint32,Uint32] :-> Uint64)
-fib_aux  = proc "fib_aux" $ \ a b n -> body $ do
+fib_rec_aux :: Def ('[Uint32,Uint32,Uint32] :-> Uint64)
+fib_rec_aux  = proc "fib_rec_aux" $ \ a b n -> body $ do
   ifte (n ==? 0)
     (ret (safeCast a))
-    (ret . safeCast =<< call fib_aux b (a + b) (n - 1))
+    (ret . safeCast =<< call fib_rec_aux b (a + b) (n - 1))
+
+-- Loop implementation of fib.
 
 fib_loop :: Def ('[Ix 1000] :-> Uint32)
 fib_loop  = proc "fib_loop" $ \ n -> body $ do
   a <- local (ival 0)
   b <- local (ival 0)
 
-  let update = (+) <$> deref a <*> deref b
-
   n `times` \ _ -> do
     store a =<< deref b
-    store b =<< update
+    store b =<< refsum a b
 
   ret =<< deref a
+  where
+  refsum :: Ref s1 (Stored Uint32) -> Ref s2 (Stored Uint32) -> Ivory eff Uint32
+  refsum refx refy = do
+    x <- deref refx
+    y <- deref refy
+    return (x + y)
+
+-- Loop implementation of fib, using a structure instead
+-- of two discrete variables.
+[ivory|
+struct Fibstate
+  { sa :: Stored Uint32
+  ; sb :: Stored Uint32
+  }
+|]
+
+fib_struct_loop :: Def ('[Ix 1000] :-> Uint32)
+fib_struct_loop  = proc "fib_struct_loop" $ \ n -> body $ do
+  state <- local (istruct [ sa .= ival 0 , sb .= ival 0 ])
+
+  let update = (+) <$> deref (state ~> sa) <*> deref (state ~> sb)
+
+  n `times` \ _ -> do
+    store (state ~> sa) =<< deref (state ~> sb)
+    store (state ~> sb) =<< update
+
+  ret =<< deref (state ~> sa)
 
 cmodule :: Module
 cmodule = package "FibLoop" $ do
-  incl fib
-  incl fib_aux
+  incl fib_rec
+  incl fib_rec_aux
   incl fib_loop
+
+  defStruct (Proxy :: Proxy "Fibstate")
+  incl fib_struct_loop
 
 runFibLoop :: IO ()
 runFibLoop  = runCompiler [cmodule] initialOpts { stdOut = True, constFold = True }
