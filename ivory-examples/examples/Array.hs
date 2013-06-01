e274b7f41f135b94d1e41484651ad65216864b7a
Pat Hickey <pat@galois.com>
2013-05-31 20:50:14 -0700

    ivory-examples: add arrayMap example

---
+++ ivory-examples/examples/Array.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+
+
+import Ivory.Interp
+import Ivory.Language
+import Ivory.Compile.C.CmdlineFrontend
+
+cmodule :: Module
+cmodule = package "ArrayExample" $ incl arrayExample
+
+runArrayExample :: IO ()
+runArrayExample = runCompiler [cmodule] initialOpts { stdOut = True }
+
+arrayExample :: Def('[Ref s (Array 4 (Stored Uint8)), Uint8] :-> ())
+arrayExample = proc "arrayExample" $ \arr n -> body $ do
+  arrayMap $ \ ix -> do
+    v <- deref (arr ! ix)
+    store (arr ! ix) (v + n)
