55a6c32c2a3b16036c6cbc271aac9afc191da5cc
Lee Pike <leepike@gmail.com>
2013-06-24 11:23:14 -0700

    ivory scope: remove unused type Writable type class and instances.

--- ivory/src/Ivory/Language/Scope.hs
+++ ivory/src/Ivory/Language/Scope.hs
@@ -2,22 +2,9 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ExistentialQuantification #-}
 
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances #-}
-
 module Ivory.Language.Scope where
 
 -- | Definition scopes for values.
 data RefScope
   = Global            -- ^ Globally allocated
   | forall s. Stack s -- ^ Stack allocated
-
--- | Determine if it's legal to write values from one allocation scope into
--- another.
-class Writable (ref :: RefScope) (val :: RefScope)
-
--- It's legal to write anything to a stack-allocated scope.
-instance Writable (Stack s) Global
-
--- It's legal to write anything to a stack-allocated scope.
-instance Writable s s
