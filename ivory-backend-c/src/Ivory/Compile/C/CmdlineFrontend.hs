e552fb3f026aff03e342927fa1a055ea0d053112
Trevor Elliott <trevor@galois.com>
2013-06-25 15:46:48 -0700

    Remove cmdargs from the c backend

--- ivory-backend-c/src/Ivory/Compile/C/CmdlineFrontend.hs
+++ ivory-backend-c/src/Ivory/Compile/C/CmdlineFrontend.hs
@@ -6,25 +6,15 @@ module Ivory.Compile.C.CmdlineFrontend
   , compileWith
   , runCompiler
   , runCompilerWith
-  , Opts(..)
+  , Opts(..), parseOpts, printUsage
   , initialOpts
   ) where
 
 import qualified Paths_ivory_backend_c
-
-import Control.Monad
-import System.Console.CmdLib
-import System.FilePath
-import qualified System.FilePath.Posix as PFP
-import System.Directory
-import System.IO (withFile, IOMode(..))
-import Data.List hiding (group)
-
-import Text.PrettyPrint.Leijen hiding ((</>), group)
-
-import Ivory.Language
 import qualified Ivory.Compile.C as C
 import qualified Ivory.Compile.C.SourceDeps as C
+
+import Ivory.Language
 import qualified Ivory.Opts.ConstFold as O
 import qualified Ivory.Opts.Overflow as O
 import qualified Ivory.Opts.DivZero as O
@@ -32,120 +22,226 @@ import qualified Ivory.Opts.Index as O
 import qualified Ivory.Opts.FP as O
 import qualified Ivory.Opts.CFG as G
 
-data Opts
-  = Opts
-    { stdOut      :: Bool
-    , includeDir  :: FilePath
-    , srcDir      :: FilePath
-    -- dependencies
-    , deps        :: [FilePath]
-    , depPrefix   :: String
-    , rtIncludeDir:: Maybe FilePath
-    -- optimization passes
-    , constFold   :: Bool
-    , overflow    :: Bool
-    , divZero     :: Bool
-    , ixCheck     :: Bool
-    , fpCheck     :: Bool
-    , outProcSyms :: Bool
-    -- CFG stuff
-    , cfg         :: Bool
-    , cfgDotDir   :: FilePath
-    , cfgProc     :: [String]
-    -- debugging
-    , verbose     :: Bool
-    } deriving (Eq, Data, Typeable)
+import Control.Monad (when)
+import Data.List (foldl')
+import Data.Monoid (Monoid(..),mconcat)
+import System.Console.GetOpt
+    (ArgOrder(Permute),OptDescr(..),ArgDescr(..),getOpt,usageInfo)
+import System.Directory (createDirectoryIfMissing)
+import System.Environment (getArgs,getProgName)
+import System.Exit (exitFailure,exitSuccess)
+import System.FilePath (takeDirectory,takeExtension,addExtension,(</>))
+import System.IO (withFile, IOMode(..), hPutStrLn)
+import Text.PrettyPrint.Mainland
+    ((<+>),(<>),line,text,stack,punctuate,render,empty,indent,displayS)
+import qualified System.FilePath.Posix as PFP
 
-instance Attributes Opts where
-  attributes _ = group "Options"
-    [ stdOut      %> [ Help "print to standard out only"
-                     , Default False
-                     ]
-    , includeDir  %> [ Help "output directory for header files"
-                     , ArgHelp "PATH"
-                     , Default "."
-                     ]
-    , srcDir      %> [ Help "output directory for source files"
-                     , ArgHelp "PATH"
-                     , Default "."
-                     ]
-    , rtIncludeDir%> [ Help "path to ivory runtime includes"
-                     , ArgHelp "PATH"
-                     , Default (Nothing :: Maybe FilePath)
-                     ]
-    , constFold   %> [ Help "constant folding."
-                     , Default False
-                     , Invertible True
-                     ]
-    , overflow    %> [ Help "generate assertions checking for arithmetic overflow/underflow."
-                     , Default False
-                     , Invertible True
-                     ]
-    , divZero     %> [ Help "generate assertions checking for division by zero."
-                     , Default False
-                     , Invertible True
-                     ]
-    , ixCheck     %> [ Help "generate assertions checking for back indexes (e.g., negative)."
-                     , Default False
-                     , Invertible True
-                     ]
-    , fpCheck     %> [ Help "generate assertions checking for NaN and Infinitiy."
-                     , Default False
-                     , Invertible True
-                     ]
-    , outProcSyms %> [ Help "write to standard out the modules' function symbols."
-                     ]
-    -- CFG stuff
-    , cfg         %> [ Help "Output control-flow graph and max stack usage."
-                     , Default False
-                     , Invertible True
-                     ]
-    , cfgDotDir   %> [ Help "output directory for CDG Graphviz file."
-                     , ArgHelp "PATH"
-                     , Default "."
-                     ]
-    , cfgProc     %> [ Help "entry function(s) for CFG computation."
-                     , ArgHelp "[proc0, proc1, ...]"
-                     , Default ([] :: [String])
-                     ]
-    , verbose     %> [ Help "verbose debugging output"
-                     , Default False
-                     , Invertible True
-                     ]
-    ]
 
-instance RecordCommand Opts where
-  mode_summary _  = "Ivory compilation frontend"
+-- Option Parsing --------------------------------------------------------------
+
+data OptParser opt
+  = Success (opt -> opt)
+  | Error [String]
+
+instance Monoid (OptParser opt) where
+  mempty                        = Success id
+
+  -- left-to-right composition makes the last option parsed take precedence
+  Success f `mappend` Success g = Success (f . g)
+  Error as  `mappend` Error bs  = Error (as ++ bs)
+  Error as  `mappend` _         = Error as
+  _         `mappend` Error bs  = Error bs
+
+-- | Option parser succeeded, use this function to transform the default
+-- options.
+success :: (opt -> opt) -> OptParser opt
+success  = Success
+
+-- | Option parser failed, emit this message.
+--
+-- XXX currently not used.
+--invalid :: String -> OptParser opt
+--invalid msg = Error [msg]
+
+
+-- | Yield either a list of errors, or a function to produce an options
+-- structure, given a set of default options.  Discard any non-options.
+parseOptions :: [OptDescr (OptParser opt)] -> [String]
+             -> (Either [String] (opt -> opt))
+parseOptions opts args = case getOpt Permute opts args of
+  (fs,[],[]) -> case mconcat fs of
+    Success f -> Right f
+    Error es  -> Left es
+  (_,_,es)  -> Left es
+
+
+
+-- Command Line Options --------------------------------------------------------
+
+data Opts = Opts
+  { stdOut      :: Bool
+  , includeDir  :: FilePath
+  , srcDir      :: FilePath
+  -- dependencies
+  , deps        :: [FilePath]
+  , depPrefix   :: String
+  , rtIncludeDir:: Maybe FilePath
+  -- optimization passes
+  , constFold   :: Bool
+  , overflow    :: Bool
+  , divZero     :: Bool
+  , ixCheck     :: Bool
+  , fpCheck     :: Bool
+  , outProcSyms :: Bool
+  -- CFG stuff
+  , cfg         :: Bool
+  , cfgDotDir   :: FilePath
+  , cfgProc     :: [String]
+  -- debugging
+  , verbose     :: Bool
+
+  , help        :: Bool
+  } deriving (Show)
 
 initialOpts :: Opts
-initialOpts = Opts
-  { stdOut      = False
-  , includeDir  = "."
-  , srcDir      = "."
-  , rtIncludeDir= Nothing
+initialOpts  = Opts
+  { stdOut       = False
+  , includeDir   = ""
+  , srcDir       = ""
+
   -- dependencies
-  , deps        = []
-  , depPrefix   = "."
+  , deps         = []
+  , depPrefix    = ""
+  , rtIncludeDir = Nothing
+
   -- optimization passes
-  , constFold   = True -- ^ Turn on constant folding by default
-  , divZero     = True -- ^ Turn on div-by-zero checking by default
-  , ixCheck     = True -- ^ Turn on index-bounds checking by default
-  , overflow    = False
-  , fpCheck     = False
-  , outProcSyms = False
+  , constFold    = False
+  , overflow     = False
+  , divZero      = False
+  , ixCheck      = False
+  , fpCheck      = False
+  , outProcSyms  = False
+
   -- CFG stuff
-  , cfg         = False
-  , cfgDotDir   = "."
-  , cfgProc     = ["main"]
+  , cfg          = False
+  , cfgDotDir    = ""
+  , cfgProc      = []
   -- debugging
-  , verbose     = False
+  , verbose      = False
+
+  , help         = False
   }
+
+setStdOut :: OptParser Opts
+setStdOut  = success (\opts -> opts { stdOut = True } )
+
+setIncludeDir :: String -> OptParser Opts
+setIncludeDir str = success (\opts -> opts { includeDir = str })
+
+setSrcDir :: String -> OptParser Opts
+setSrcDir str = success (\opts -> opts { srcDir = str })
+
+setRtIncludeDir :: String -> OptParser Opts
+setRtIncludeDir str = success (\opts -> opts { rtIncludeDir = Just str })
+
+setConstFold :: OptParser Opts
+setConstFold  = success (\opts -> opts { constFold = True })
+
+setOverflow :: OptParser Opts
+setOverflow  = success (\opts -> opts { overflow = True })
+
+setDivZero :: OptParser Opts
+setDivZero  = success (\opts -> opts { divZero = True })
+
+setIxCheck :: OptParser Opts
+setIxCheck  = success (\opts -> opts { ixCheck = True })
+
+setFpCheck :: OptParser Opts
+setFpCheck  = success (\opts -> opts { fpCheck = True })
+
+setProcSyms :: OptParser Opts
+setProcSyms  = success (\opts -> opts { outProcSyms = True })
+
+setCfg :: OptParser Opts
+setCfg  = success (\opts -> opts { cfg = True })
+
+setCfgDotDir :: String -> OptParser Opts
+setCfgDotDir str = success (\opts -> opts { cfgDotDir = str })
+
+addCfgProc :: String -> OptParser Opts
+addCfgProc str = success (\opts -> opts { cfgProc = cfgProc opts ++ [str] })
+
+setVerbose :: OptParser Opts
+setVerbose  = success (\opts -> opts { verbose = True })
+
+setHelp :: OptParser Opts
+setHelp  = success (\opts -> opts { help = True })
+
+options :: [OptDescr (OptParser Opts)]
+options  =
+  [ Option "" ["std-out"] (NoArg setStdOut)
+    "print to stdout only"
+  , Option "" ["include-dir"] (ReqArg setIncludeDir "PATH")
+    "output directory for header files"
+  , Option "" ["src-dir"] (ReqArg setSrcDir "PATH")
+    "output directory for source files"
+  , Option "" ["rt-include-dir"] (ReqArg setRtIncludeDir "PATH")
+    "path to ivory runtime includes"
+
+  , Option "" ["const-fold"] (NoArg setConstFold)
+    "enable the constant folding pass"
+  , Option "" ["overflow"] (NoArg setOverflow)
+    "enable overflow checking annotations"
+  , Option "" ["div-zero"] (NoArg setDivZero)
+    "generate assertions checking for division by zero."
+  , Option "" ["ix-check"] (NoArg setIxCheck)
+    "generate assertions checking for back indexes (e.g., negative)"
+  , Option "" ["fp-check"] (NoArg setFpCheck)
+    "generate assertions checking for NaN and Infinitiy."
+
+  , Option "" ["out-proc-syms"] (NoArg setProcSyms)
+    "dump out the modules' function symbols"
+
+  , Option "" ["cfg"] (NoArg setCfg)
+    "Output control-flow graph and max stack usage."
+
+  , Option "" ["cfg-dot-dir"] (ReqArg setCfgDotDir "PATH")
+    "output directory for CFG Graphviz file"
+  , Option "" ["cfg-proc"] (ReqArg addCfgProc "NAME")
+    "entry function(s) for CFG computation."
+
+  , Option "" ["verbose"] (NoArg setVerbose)
+    "verbose debugging output"
+
+  , Option "h" ["help"] (NoArg setHelp)
+    "display this message"
+  ]
+
+-- | Parse an @Opts@ structure from a list of strings.
+parseOpts :: [String] -> IO Opts
+parseOpts args = case parseOptions options args of
+  Right f   ->
+    let opts = f initialOpts
+     in if help opts
+           then printUsage [] >> exitSuccess
+           else return opts
+  Left errs -> printUsage errs >> exitFailure
+
+printUsage :: [String] -> IO ()
+printUsage errs = do
+  prog <- getProgName
+  let banner = unlines
+        (errs ++ ["", "Usage: " ++ prog ++ " [OPTIONS]"])
+  putStrLn (usageInfo banner options)
+
+
+-- Code Generation Front End ---------------------------------------------------
 
 compile :: [Module] -> IO ()
 compile = compileWith Nothing Nothing
 
 compileWith :: Maybe G.SizeMap -> Maybe [IO FilePath] -> [Module] -> IO ()
-compileWith sm sp ms = runCompilerWith sm sp ms =<< executeR initialOpts =<< getArgs
+compileWith sm sp ms = runCompilerWith sm sp ms =<< parseOpts =<< getArgs
 
 runCompilerWith :: Maybe G.SizeMap -> Maybe [IO FilePath] -> [Module] -> Opts -> IO ()
 runCompilerWith sm sp =
@@ -256,10 +352,9 @@ outputDeps [path] prefix headers sources = do
   createDirectoryIfMissing True (takeDirectory path)
   withFile path WriteMode writedoc
   where
-  writedoc h = displayIO h rendered
-  rendered = renderPretty 1.0 w d
+  writedoc h = hPutStrLn h (displayS (render w d) "")
   w = 10000000 -- don't ever wrap lines - invalid make syntax
-  d = vsep $
+  d = stack $
     [ text "# dep file autogenerated by ivory compiler"
     , empty
     , listof (prefix ++ "_HEADERS") headers
@@ -268,7 +363,7 @@ outputDeps [path] prefix headers sources = do
     ]
   declaration n = text n <+> text ":= \\" <> line
   listof name values = declaration name <>
-    (indent 4 $ vsep $ punctuate (text " \\") (map text values))
+    (indent 4 $ stack $ punctuate (text " \\") (map text values))
 outputDeps _ _ _ _ = error "invalid dep path (should be prevented by caller)"
 
 
