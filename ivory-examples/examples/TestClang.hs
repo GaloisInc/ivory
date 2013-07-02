7a2667af1b12d3514ef81ea01c5bfa991e0fd612
Pat Hickey <pat@galois.com>
2013-07-02 15:31:54 -0700

    ivory-examples: add regression test used to narrow addrOf bug fixed in prev commit

--- ivory-examples/examples/TestClang.hs
+++ ivory-examples/examples/TestClang.hs
@@ -14,6 +14,7 @@ import qualified Forever
 import qualified PublicPrivate
 import qualified Bits
 import qualified SizeOf
+import qualified AddrOfRegression
 
 import Ivory.Compile.C.CmdlineFrontend
 import Ivory.Language (Module(),moduleName)
@@ -50,4 +51,5 @@ modules = [ PID.cmodule
           , PublicPrivate.cmodule
           , Bits.cmodule
           , SizeOf.cmodule
+          , AddrOfRegression.cmodule
           ]
