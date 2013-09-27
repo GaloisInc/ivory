cc551555a5e1cec55383ce55e7693cd82256fa8d
Lee Pike <leepike@gmail.com>
2013-09-27 16:06:10 -0700

    ivory-backend: remove shadow warning.

--- ivory-backend-c/src/Ivory/Compile/C/Prop.hs
+++ ivory-backend-c/src/Ivory/Compile/C/Prop.hs
@@ -13,16 +13,16 @@ ensTrans :: I.Expr -> I.Expr -> I.Expr
 ensTrans retE = loop
   where
   loop e = case e of
-    I.ExpSym{}            -> e
-    I.ExpVar v            -> if v == I.retval then retE else e
-    I.ExpLit{}            -> e
-    I.ExpOp op args       -> I.ExpOp op (map loop args)
-    I.ExpLabel t e0 s     -> I.ExpLabel t (loop e0) s
-    I.ExpIndex t e0 t1 e1 -> I.ExpIndex t (loop e0) t1 (loop e1)
-    I.ExpSafeCast t e0    -> I.ExpSafeCast t (loop e0)
-    I.ExpToIx e0 maxSz    -> I.ExpToIx (loop e0) maxSz
-    I.ExpAddrOfGlobal{}   -> e
-    I.ExpDynArrayLength e -> I.ExpDynArrayLength (loop e)
-    I.ExpDynArrayData t e -> I.ExpDynArrayData t (loop e)
+    I.ExpSym{}             -> e
+    I.ExpVar v             -> if v == I.retval then retE else e
+    I.ExpLit{}             -> e
+    I.ExpOp op args        -> I.ExpOp op (map loop args)
+    I.ExpLabel t e0 s      -> I.ExpLabel t (loop e0) s
+    I.ExpIndex t e0 t1 e1  -> I.ExpIndex t (loop e0) t1 (loop e1)
+    I.ExpSafeCast t e0     -> I.ExpSafeCast t (loop e0)
+    I.ExpToIx e0 maxSz     -> I.ExpToIx (loop e0) maxSz
+    I.ExpAddrOfGlobal{}    -> e
+    I.ExpDynArrayLength e0 -> I.ExpDynArrayLength (loop e0)
+    I.ExpDynArrayData t e0 -> I.ExpDynArrayData t (loop e0)
 
 --------------------------------------------------------------------------------
