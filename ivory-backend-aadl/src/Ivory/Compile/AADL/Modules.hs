c5f80b5c0527c13d89e0f163b17d33de7c9e28cd
Pat Hickey <pat@galois.com>
2013-11-08 16:28:56 -0800

    ivory-backend-aadl: extend Monad with scope (naming) tracking, and warning system

--- ivory-backend-aadl/src/Ivory/Compile/AADL/Modules.hs
+++ ivory-backend-aadl/src/Ivory/Compile/AADL/Modules.hs
@@ -13,13 +13,14 @@ import Ivory.Compile.AADL.Monad
 --------------------------------------------------------------------------------
 
 -- | Compile a module.
-compileModule :: [I.Module] -> I.Module -> Maybe Document
-compileModule modulectx mod = case doc of
+compileModule :: [I.Module] -> I.Module -> Maybe (Document, [Warning])
+compileModule modulectx mod =
+  case doc of
     Document _ _ [] -> Nothing
-    _               -> Just doc
+    _               -> Just (doc, warnings)
   where
   structs = I.modStructs mod
-  doc = runCompile modulectx mod $ do
+  (doc, warnings) = runCompile modulectx mod $ do
     mapM_ compileStruct (I.public structs)
     mapM_ compileStruct (I.private structs)
 
