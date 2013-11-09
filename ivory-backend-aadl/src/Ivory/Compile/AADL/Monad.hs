05effe2ec9934076552fe89a9fdf7e4a81cddd3b
Pat Hickey <pat@galois.com>
2013-11-08 16:28:56 -0800

    ivory-backend-aadl: extend Monad with scope (naming) tracking, and warning system

--- ivory-backend-aadl/src/Ivory/Compile/AADL/Monad.hs
+++ ivory-backend-aadl/src/Ivory/Compile/AADL/Monad.hs
@@ -11,22 +11,34 @@ import Ivory.Compile.AADL.Identifier
 import qualified Ivory.Language.Syntax as I
 
 newtype CompileM a = Compile
-  { unCompile :: StateT Document (ReaderT (I.Module,[I.Module]) Id) a }
+  { unCompile :: StateT Document                 -- Building compiled document
+                  (ReaderT (I.Module,[I.Module]) -- Ivory module Environment
+                    (StateT [(String,[String])]  -- Scope name and identifiers
+                      (WriterT [Warning]         -- Log of warnings
+                      Id))) a }
   deriving (Functor, Monad)
+
+data Warning
+  = UniquenessWarning String [String] -- msg, scope
 
 type Compile = CompileM ()
 
 -- Using snoc to write to lists, reverse at end of run.
-runCompile :: [I.Module] -> I.Module -> Compile -> Document
-runCompile allms m (Compile c) = Document
-  { doc_name        = identifier (I.modName m)
-  , doc_imports     = reverse (doc_imports d)
-  , doc_definitions = reverse (doc_definitions d)
-  }
+runCompile :: [I.Module] -> I.Module -> Compile -> (Document, [Warning])
+runCompile allms m (Compile c) = (reversedDoc, ws)
   where
+  dname = identifier (I.modName m)
+  reversedDoc = Document
+    { doc_name        = identifier (I.modName m)
+    , doc_imports     = reverse (doc_imports d)
+    , doc_definitions = reverse (doc_definitions d)
+    }
   modulectx = (m, allms)
-  d :: Document
-  d = snd (runId (runReaderT modulectx (runStateT mempty c)))
+  (((_retv,d),_scope),ws) = runId
+                          $ runWriterT
+                          $ runStateT [(dname ++ " document",[])]
+                          $ runReaderT modulectx
+                          $ runStateT mempty c
 
 writeTypeDefinition :: DTypeDef -> Compile
 writeTypeDefinition dtdef = Compile $ do
@@ -60,4 +72,37 @@ qualTypeName q n = do
 
 getIModContext :: CompileM (I.Module,[I.Module])
 getIModContext = Compile $ lift ask
+
+getScope :: CompileM [(String, [String])]
+getScope = Compile $ lift get
+
+setScope :: [(String,[String])] -> CompileM ()
+setScope s = Compile $ lift $ set s
+
+innerScope :: String -> CompileM a -> CompileM a
+innerScope name k = do
+  s <- getScope
+  setScope ((name, []):s)
+  r <- k
+  setScope s
+  return r
+
+existsInScope :: String -> CompileM Bool
+existsInScope identifier = do
+  s <- getScope
+  return $ or $ map (elem identifier . snd) s
+
+addToScope :: String -> CompileM ()
+addToScope identifier = do
+  ((name,s):ss) <- getScope
+  setScope ((name,(identifier:s)):ss)
+
+writeWarning :: Warning -> CompileM ()
+writeWarning w = Compile $ put [w]
+
+uniquenessWarning :: String -> CompileM ()
+uniquenessWarning msg = do
+  scopes <- getScope
+  writeWarning $ UniquenessWarning msg (map fst scopes)
+
 
