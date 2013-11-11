22625ec518723e3d278f869686746f235deb7db5
Lee Pike <leepike@gmail.com>
2013-11-11 14:33:24 -0800

    examples: add regression test for conditional assertions.

--- ivory-examples/examples/Cond.hs
+++ ivory-examples/examples/Cond.hs
@@ -4,6 +4,8 @@
 module Cond where
 
 import Ivory.Language
+import Ivory.Compile.C.CmdlineFrontend
+import Ivory.Compile.C.Modules
 
 
 add :: Def ('[Uint32,Uint32] :-> Uint32)
@@ -14,3 +16,20 @@ add  = proc "add"
 
 cmodule :: Module
 cmodule = package "cond" $ incl add
+
+-- Testing assertions with choice expression
+
+foo :: Def ('[IFloat,IFloat,IFloat] :-> IFloat)
+foo = proc "foo" $ \x y z -> body $ do
+  let cond  = 2/x ==? 5
+  let tCond = 6/y ==? 7
+  let exp   = tCond ? (8/z, 9)
+  ret (cond ? (exp,4))
+
+fooMod :: Module
+fooMod  = package "fooM" $ incl foo
+
+runFoo :: IO ()
+runFoo = runCompiler [fooMod] initialOpts { stdOut = True
+                                          , divZero = True
+                                          }
