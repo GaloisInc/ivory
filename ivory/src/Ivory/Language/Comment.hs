070966e758df81d99d4e0971c6cd10eb30199fed
Pat Hickey <pat@galois.com>
2014-04-05 19:19:55 -0700

    ivory: add comment to interface & ast

---
+++ ivory/src/Ivory/Language/Comment.hs
@@ -0,0 +1,8 @@
+
+module Ivory.Language.Comment where
+
+import Ivory.Language.Monad
+import qualified Ivory.Language.Syntax as AST
+
+comment :: String -> Ivory eff ()
+comment c = emit (AST.Comment c)
