1e4ca6444697efaf1236545e1478f8f1518763a8
leepike <leepike@gmail.com>
2014-04-07 17:07:07 -0700

    parser: auto generate Ivory modules based on the module name.

--- ivory/src/Ivory/Language/CSyntax/QQ.hs
+++ ivory/src/Ivory/Language/CSyntax/QQ.hs
@@ -14,12 +14,17 @@ module Ivory.Language.CSyntax.QQ   ( c ) where
 
 import           Prelude hiding (exp, init)
 import qualified Prelude as P
+import           Data.Char
+import           Data.List
 
-import Ivory.Language.CSyntax.QQ.ProcQQ
-
-import           Language.Haskell.TH()
+import qualified Language.Haskell.TH       as Q
+import           Language.Haskell.TH       hiding (Stmt, Exp, Type)
 import           Language.Haskell.TH.Quote
 
+import qualified Ivory.Language as I
+
+import Ivory.Language.CSyntax.QQ.ProcQQ
+import Ivory.Language.CSyntax.ParseAST
 import Ivory.Language.CSyntax.Parser
 
 --------------------------------------------------------------------------------
@@ -34,12 +39,57 @@ c = QuasiQuoter
   }
   where
   decP str = do
-    procs <- ivoryCParser str
-    cDefs <- mapM fromProc procs
-    return (concat cDefs)
+    procs         <- ivoryCParser str
+    cDefs         <- mapM fromProc procs
+    let procSyms  = foldr toGlobalSym [] procs
+    ivoryModDefs  <- ivoryMod procSyms
+    return (concat cDefs ++ ivoryModDefs)
   err str = error $ str ++ " not implemented for c quasiquoter."
 
 --------------------------------------------------------------------------------
+
+-- | Add a procedure symbol to the global symbols.
+toGlobalSym :: ProcDef -> [GlobalSym] -> [GlobalSym]
+toGlobalSym procS = (ProcSym (procSym procS) :)
+
+-- | Include an Ivory symbol into the Ivory module.
+ivorySymMod :: GlobalSym -> Q.Exp
+ivorySymMod sym = case sym of
+  ProcSym procName   -> AppE (VarE 'I.incl) (VarE $ mkName procName)
+
+-- | Define an Ivory module, one per module.
+ivoryMod :: [GlobalSym] -> Q [Dec]
+ivoryMod incls = do
+  modTy  <- mkModTy
+  mi <- modImp
+  return [modTy, mi]
+  where
+  modImp :: Q Dec
+  modImp = do
+    nm <- modNameQ
+    bd <- modBody
+    return $ ValD (VarP $ mkName nm)
+                  (NormalB bd)
+                  []
+  -- Module names are uppercase and have full context.  Turn Foo.Bar.FooBar into
+  -- foobar for the Ivory module name.
+  mkValidHsVar []      = error "Empty module name in ivoryMod!"
+  mkValidHsVar fullMod = map toLower
+    $ reverse (takeWhile (/= '.') (reverse fullMod))
+
+  modNameQ = location >>= (return . mkValidHsVar . loc_module)
+  modBody = do
+    nm <- stringE =<< modNameQ
+    let pkg   = AppE (VarE 'I.package) nm
+    let doblk = map (NoBindS . ivorySymMod) incls
+    return (AppE pkg (DoE doblk))
+
+  mkModTy = do
+    nm <- modNameQ
+    return $ SigD (mkName nm) (ConT ''I.Module)
+
+--------------------------------------------------------------------------------
+
 -- Testing
 
 {-
