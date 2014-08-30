a78fc805bc60f65e132e772f8d4c23209472244d
leepike <leepike@gmail.com>
2014-08-30 12:08:56 +0200

    concrete syntax: clean up examples.

---
+++ ivory-examples/examples/ConcreteFile.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+--
+-- C-like syntax for Ivory, parsed from a file.
+--
+-- Copyright (C) 2014, Galois, Inc.
+-- All rights reserved.
+--
+
+module Concrete where
+
+import Prelude hiding (return)
+import Control.Monad (void)
+import Ivory.Language
+import Ivory.Compile.C.CmdlineFrontend
+
+
+e :: IBool
+e = (4::Sint32) >? 3
+
+type SomeInt = Uint32
+
+macroStmts x y = do
+  a <- local (ival 0)
+  store a (x + y)
+
+macroExp x y = do
+  x <? y
+
+[ivoryFile|examples/file.ivory|]
+
+runFile :: IO ()
+runFile = void $ runCompiler [examplesfile] initialOpts {stdOut = True, constFold = True}
