942bb1581c9771bd0465c481a57c7221be0fd31c
leepike <leepike@gmail.com>
2014-10-27 16:20:57 -0700

    concrete syntax: fix bug in parsing integers/floats.

--- ivory/src/Ivory/Language/Syntax/Concrete/Lexeme.hs
+++ ivory/src/Ivory/Language/Syntax/Concrete/Lexeme.hs
@@ -36,15 +36,25 @@ data Token =
 readBitLit :: String -> Token
 readBitLit s =
   let (width, val) = break (== 'b') s in
-  TokBitLit (read width, readBin (tail val))
+  let w = case reads width of
+            [(i,"")] -> i
+            _        -> error $ "Lex error on bitWidth " ++ width
+  in
+  TokBitLit (w, readBin (tail val))
 
 -- If Alex calls readBin, a lex error should be impossible.
 readBin :: (Show a, Eq a, Num a) => String -> a
 readBin s =
   case readInt 2 (`elem` "01") digitToInt s of
     [(v,"")] -> v
-    ls       -> error $ "Impossible lex error on " ++ show ls
+    ls       -> error $ "Impossible lex error on binary integer " ++ show ls
 
+readInteger :: String -> Token
+readInteger s = case reads s of
+  [(i,"")] -> TokInteger i
+  _        -> error $ "Lex error on integer " ++ s
 
-
-
+readFloat :: String -> Token
+readFloat s = case reads s of
+  [(f,"")] -> TokFloat f
+  _        -> error $ "Lex error on float " ++ s
