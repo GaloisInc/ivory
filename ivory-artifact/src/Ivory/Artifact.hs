ac7d8f230b2bf814d53e6fe3dd43224fb85d5676
Pat Hickey <pat@galois.com>
2014-11-04 14:07:40 -0800

    ivory-artifact: hide constructors, expose artifactFileName accessor, haddock

--- ivory-artifact/src/Ivory/Artifact.hs
+++ ivory-artifact/src/Ivory/Artifact.hs
@@ -1,11 +1,56 @@
-module Ivory.Artifact
-  ( Artifact(..)
-  , AContents(..)
+module Ivory.Artifact (
+  -- * Ivory Artifacts
+  -- | Artifacts permit the user to generate non-Ivory language files
+  -- as part of an Ivory program's build process. Artifacts are typically
+  -- used to encapsulate helper code (such as Ivory.Stdlib's string functions
+  -- helper .c and .h files) or to generate debug or metadata output. The
+  -- contents of an `Artifact` are given by some external file, or as a string.
+  --
+  -- Artifacts are exposed as an abstract type `Artifact` with a set of
+  -- constructors, an accessor `artifactFileName`, and two functions,
+  -- `putArtifacts` and `printArtifacts`, which are used to write a set of
+  -- Artifacts to a file or print them to stdout, respectively.
+  --
+    Artifact()
+  -- | Gives the file name that will be used when writing the `Artifact` to the
+  -- filesystem.
+  , artifactFileName
+
+  -- * Artifact constructors
+  -- | `artifactFile` creates an `Artifact` from an output filename of type
+  -- `FilePath` and an input filepath of type `IO FilePath`. The output filename
+  -- should be a simple filename, any directory information will be dropped. The
+  -- input filepath should be an absolute path. The input filepath is in the IO
+  -- monad for compatiblility with Cabal's `getDataDir` data files functionality.
   , artifactFile
+
+  -- | `artifactCabalFile` creates an `Artifact` given an input directory of
+  -- type `IO FilePath` and an input filepath, relative to the input directory
+  -- in the preceeding argument, of type `FilePath`.
+  --
+  -- This function is designed to be used with a Cabal Paths_(packagename)
+  -- `getDataDir`, which has type `IO FilePath`, and a path to a file inside the
+  -- data directory.  It is implemented in terms of `artifactFileName`. The
+  -- output filename is given by dropping directory information from the input
+  -- filepath.
   , artifactCabalFile
+
+  -- | `artifactText` creates an `Artifact` from an output filename of type
+  -- `FilePath` and with contents of type `Text`.
   , artifactText
+  -- | `artifactString` creates an `Artifact` from an output filename of type
+  -- `FilePath` with contents of type `String`. Implemented in terms of
+  -- `artifactText`.
   , artifactString
+
+  -- * Artifact actions
+  -- | Takes a directory of type `FilePath` and a list of `Artifact`s, and
+  -- writes each Artifact to the filesystem. Gives a `Maybe String` containing
+  -- any errors encountered when an `Artifact` is specified by an input filename
+  -- which does not exist.
   , putArtifacts
+
+  -- | Takes a list of `Artifact`s and prints them to stdout.
   , printArtifacts
   ) where
 
@@ -20,6 +65,8 @@ data Artifact = Artifact FilePath AContents
 data AContents = LiteralContents T.Text
                | FileContents (IO FilePath)
 
+artifactFileName :: Artifact -> FilePath
+artifactFileName (Artifact f _ ) = f
 
 artifactFile :: FilePath -> IO FilePath -> Artifact
 artifactFile outputname inputpath =
@@ -27,8 +74,8 @@ artifactFile outputname inputpath =
 
 artifactCabalFile :: IO FilePath -> FilePath -> Artifact
 artifactCabalFile inputdir inputfname =
-  Artifact (takeFileName inputfname)
-    (FileContents (fmap (\i -> (i </> inputfname)) inputdir))
+  artifactFile (takeFileName inputfname)
+               (fmap (\i -> (i </> inputfname)) inputdir)
 
 artifactText :: FilePath -> T.Text -> Artifact
 artifactText outputname t =
