9e3b1a4ca47b100f50e9ae562df32f020c38813b
Pat Hickey <pat@galois.com>
2014-11-03 18:45:49 -0800

    ivory-hw: switch from sourceDep to Artifacts

--- ivory-hw/src/Ivory/HW/Module.hs
+++ ivory-hw/src/Ivory/HW/Module.hs
@@ -8,8 +8,12 @@
 module Ivory.HW.Module where
 
 import Ivory.Language
+import Ivory.Artifact
+import qualified Paths_ivory_hw
 
 hw_moduledef :: ModuleDef
-hw_moduledef = do
-  inclHeader "ivory_hw_prim.h"
-  sourceDep  "ivory_hw_prim.h"
+hw_moduledef = inclHeader "ivory_hw_prim.h"
+
+hw_artifacts :: [Artifact]
+hw_artifacts =
+  [artifactCabalFile Paths_ivory_hw.getDataDir "support/ivory_hw_prim.h"]
