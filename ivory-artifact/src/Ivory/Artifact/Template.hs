442f1b10ca7b617947a08e25a465a4f94db509c1
Pat Hickey <pat@galois.com>
2014-11-06 17:20:24 -0800

    ivory-artifact: add HStringTemplate based templating

---
+++ ivory-artifact/src/Ivory/Artifact/Template.hs
@@ -0,0 +1,36 @@
+
+module Ivory.Artifact.Template
+  ( artifactCabalFileTemplate
+  ) where
+
+import Ivory.Artifact
+import Text.StringTemplate
+import System.FilePath
+
+artifactCabalFileTemplate :: IO FilePath -> FilePath -> [(String, String)]
+                          -> Artifact
+artifactCabalFileTemplate datadir templatepath attrs =
+  artifactTransformErrString applyconf af
+  where
+  af = artifactFile outputpath (fmap (\f -> f </> templatepath) datadir)
+  templatename = takeFileName templatepath
+  outputpath = case splitExtension templatename of
+    (a, ext) | ext == ".template" -> a
+    _ -> templatename
+
+  applyconf s =
+    let t  = newSTMP s :: StringTemplate String
+        t' = setManyAttrib attrs t
+    in case checkTemplate t' of
+      (Just e, _, _) -> Left (parseErr e)
+      (_, Just e, _) -> Left (missingAttrErr e)
+      (_, _, Just e) -> Left (missingTemplate e)
+      (_, _, _) -> Right (toString t')
+  prefix = "Error in " ++ templatename ++ ": "
+  parseErr e = prefix ++ "Failed to parse: \n" ++ e
+  missingAttrErr es =  prefix ++ "The following attributes are missing:\n"
+                    ++ unlines es
+  missingTemplate es =  prefix ++ "Failed to lookup invoked templates: \n"
+                     ++ unlines es
+
+
