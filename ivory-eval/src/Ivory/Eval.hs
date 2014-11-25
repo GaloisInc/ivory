d47eb79c0b5013dc965d91529a58995b51823d92
Eric Seidel <gridaphobe@gmail.com>
2014-11-25 10:32:16 -0800

    more numerics

--- ivory-eval/src/Ivory/Eval.hs
+++ ivory-eval/src/Ivory/Eval.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 
 module Ivory.Eval where
@@ -10,6 +11,7 @@ import qualified Data.Map as M
 import Data.Word
 import MonadLib.Monads
 import Ivory.Language.Syntax.Concrete.Location
+import qualified Ivory.Language.Array as I
 import qualified Ivory.Language.Syntax as I
 
 -- XXX: DEBUG
@@ -31,7 +33,15 @@ initState st = EvalState st NoLoc
 
 data Value
   = Sint8  Int8
+  | Sint16 Int16
+  | Sint32 Int32
+  | Sint64 Int64
   | Uint8  Word8
+  | Uint16 Word16
+  | Uint32 Word32
+  | Uint64 Word64
+  | Float  Float
+  | Double Double
   | Char   Char
   | String String
   | Bool   Bool
@@ -58,59 +68,95 @@ or :: Value -> Value -> Value
 or (Bool x) (Bool y) = Bool (x || y)
 or x        y        = error $ "invalid operors to `or`: " ++ show (x,y)
 
-gt :: Value -> Value -> Value
-(Sint8 x) `gt` (Sint8 y) = Bool (x > y)
-(Uint8 x) `gt` (Uint8 y) = Bool (x > y)
-(Char x)  `gt` (Char y)  = Bool (x > y)
-x         `gt` y         = error $ "invalid operands to `gt`: " ++ show (x,y)
+ordOp :: (forall a. Ord a => a -> a -> Bool) -> Value -> Value -> Value
+ordOp op (Sint8  x) (Sint8  y) = Bool (op x y)
+ordOp op (Sint16 x) (Sint16 y) = Bool (op x y)
+ordOp op (Sint32 x) (Sint32 y) = Bool (op x y)
+ordOp op (Sint64 x) (Sint64 y) = Bool (op x y)
+ordOp op (Uint8  x) (Uint8  y) = Bool (op x y)
+ordOp op (Uint16 x) (Uint16 y) = Bool (op x y)
+ordOp op (Uint32 x) (Uint32 y) = Bool (op x y)
+ordOp op (Uint64 x) (Uint64 y) = Bool (op x y)
+ordOp op (Float  x) (Float  y) = Bool (op x y)
+ordOp op (Double x) (Double y) = Bool (op x y)
+ordOp op (Char   x) (Char   y) = Bool (op x y)
+ordOp _ x y = error $ "invalid operands to `ordOp`: " ++ show (x,y)
+
+gt  :: Value -> Value -> Value
+gt  = ordOp (>)
 
 gte :: Value -> Value -> Value
-(Sint8 x) `gte` (Sint8 y) = Bool (x >= y)
-(Uint8 x) `gte` (Uint8 y) = Bool (x >= y)
-(Char x)  `gte` (Char y)  = Bool (x >= y)
-x         `gte` y         = error $ "invalid operands to `gte`: " ++ show (x,y)
+gte = ordOp (>=)
 
-lt :: Value -> Value -> Value
-(Sint8 x) `lt` (Sint8 y) = Bool (x < y)
-(Uint8 x) `lt` (Uint8 y) = Bool (x < y)
-(Char x)  `lt` (Char y)  = Bool (x < y)
-x         `lt` y         = error $ "invalid operands to `gt`: " ++ show (x,y)
+lt  :: Value -> Value -> Value
+lt  = ordOp (<)
 
 lte :: Value -> Value -> Value
-(Sint8 x) `lte` (Sint8 y) = Bool (x <= y)
-(Uint8 x) `lte` (Uint8 y) = Bool (x <= y)
-(Char x)  `lte` (Char y)  = Bool (x <= y)
-x         `lte` y         = error $ "invalid operands to `lte`: " ++ show (x,y)
+lte = ordOp (<=)
 
-add :: Value -> Value -> Value
-(Sint8 x) `add` (Sint8 y) = Sint8 (x + y)
-(Uint8 x) `add` (Uint8 y) = Uint8 (x + y)
-x         `add` y         = error $ "invalid operands to `add`: " ++ show (x,y)
-
-sub :: Value -> Value -> Value
-(Sint8 x) `sub` (Sint8 y) = Sint8 (x - y)
-(Uint8 x) `sub` (Uint8 y) = Uint8 (x - y)
-x         `sub` y         = error $ "invalid operands to `sub`: " ++ show (x,y)
-
-mul :: Value -> Value -> Value
-(Sint8 x) `mul` (Sint8 y) = Sint8 (x * y)
-(Uint8 x) `mul` (Uint8 y) = Uint8 (x * y)
-x         `mul` y         = error $ "invalid operands to `mul`: " ++ show (x,y)
-
-div :: Value -> Value -> Value
-(Sint8 x) `div` (Sint8 y) = Sint8 (x `Prelude.div` y)
-(Uint8 x) `div` (Uint8 y) = Uint8 (x `Prelude.div` y)
-x         `div` y         = error $ "invalid operands to `div`: " ++ show (x,y)
-
-mod :: Value -> Value -> Value
-(Sint8 x) `mod` (Sint8 y) = Sint8 (x `Prelude.mod` y)
-(Uint8 x) `mod` (Uint8 y) = Uint8 (x `Prelude.mod` y)
-x         `mod` y         = error $ "invalid operands to `mod`: " ++ show (x,y)
+numUnOp :: (forall a. Num a => a -> a) -> Value -> Value
+numUnOp op (Sint8  x) = Sint8  (op x)
+numUnOp op (Sint16 x) = Sint16 (op x)
+numUnOp op (Sint32 x) = Sint32 (op x)
+numUnOp op (Sint64 x) = Sint64 (op x)
+numUnOp op (Uint8  x) = Uint8  (op x)
+numUnOp op (Uint16 x) = Uint16 (op x)
+numUnOp op (Uint32 x) = Uint32 (op x)
+numUnOp op (Uint64 x) = Uint64 (op x)
+numUnOp op (Float  x) = Float  (op x)
+numUnOp op (Double x) = Double (op x)
+numUnOp _ x = error $ "invalid operands to `numUnOp`: " ++ show x
 
 negate :: Value -> Value
-negate (Sint8 x) = Sint8 (Prelude.negate x)
-negate (Uint8 x) = Uint8 (Prelude.negate x)
-negate x         = error $ "invalid operands to `negate`: " ++ show x
+negate = numUnOp Prelude.negate
+
+numBinOp :: (forall a. Num a => a -> a -> a) -> Value -> Value -> Value
+numBinOp op (Sint8  x) (Sint8  y) = Sint8  (op x y)
+numBinOp op (Sint16 x) (Sint16 y) = Sint16 (op x y)
+numBinOp op (Sint32 x) (Sint32 y) = Sint32 (op x y)
+numBinOp op (Sint64 x) (Sint64 y) = Sint64 (op x y)
+numBinOp op (Uint8  x) (Uint8  y) = Uint8  (op x y)
+numBinOp op (Uint16 x) (Uint16 y) = Uint16 (op x y)
+numBinOp op (Uint32 x) (Uint32 y) = Uint32 (op x y)
+numBinOp op (Uint64 x) (Uint64 y) = Uint64 (op x y)
+numBinOp op (Float  x) (Float  y) = Float  (op x y)
+numBinOp op (Double x) (Double y) = Double (op x y)
+numBinOp _ x y = error $ "invalid operands to `numBinOp`: " ++ show (x,y)
+
+add :: Value -> Value -> Value
+add = numBinOp (+)
+
+sub :: Value -> Value -> Value
+sub = numBinOp (-)
+
+mul :: Value -> Value -> Value
+mul = numBinOp (*)
+
+div :: Value -> Value -> Value
+(Sint8  x) `div` (Sint8  y) = Sint8  (x `Prelude.div` y)
+(Sint16 x) `div` (Sint16 y) = Sint16 (x `Prelude.div` y)
+(Sint32 x) `div` (Sint32 y) = Sint32 (x `Prelude.div` y)
+(Sint64 x) `div` (Sint64 y) = Sint64 (x `Prelude.div` y)
+(Uint8  x) `div` (Uint8  y) = Uint8  (x `Prelude.div` y)
+(Uint16 x) `div` (Uint16 y) = Uint16 (x `Prelude.div` y)
+(Uint32 x) `div` (Uint32 y) = Uint32 (x `Prelude.div` y)
+(Uint64 x) `div` (Uint64 y) = Uint64 (x `Prelude.div` y)
+(Float  x) `div` (Float  y) = Float  (x / y)
+(Double x) `div` (Double y) = Double (x / y)
+x          `div` y          = error $ "invalid operands to `div`: " ++ show (x,y)
+
+mod :: Value -> Value -> Value
+(Sint8  x) `mod` (Sint8  y) = Sint8  (x `Prelude.mod` y)
+(Sint16 x) `mod` (Sint16 y) = Sint16 (x `Prelude.mod` y)
+(Sint32 x) `mod` (Sint32 y) = Sint32 (x `Prelude.mod` y)
+(Sint64 x) `mod` (Sint64 y) = Sint64 (x `Prelude.mod` y)
+(Uint8  x) `mod` (Uint8  y) = Uint8  (x `Prelude.mod` y)
+(Uint16 x) `mod` (Uint16 y) = Uint16 (x `Prelude.mod` y)
+(Uint32 x) `mod` (Uint32 y) = Uint32 (x `Prelude.mod` y)
+(Uint64 x) `mod` (Uint64 y) = Uint64 (x `Prelude.mod` y)
+-- (Float  x) `mod` (Float  y) = Float  (x `Prelude.mod` y)
+-- (Double x) `mod` (Double y) = Double (x `Prelude.mod` y)
+x          `mod` y          = error $ "invalid operands to `mod`: " ++ show (x,y)
 
 readStore :: I.Sym -> Eval Value
 readStore sym = do
@@ -143,20 +189,20 @@ evalStmt stmt = case stmt of
   I.CompilerAssert expr
     -> evalAssert expr
   I.IfTE cond true false
-    -> do b <- evalExpr cond
+    -> do b <- evalExpr I.TyBool cond
           case b of
             Bool True  -> evalBlock true
             Bool False -> evalBlock false
             _          -> raise $ "evalStmt: IfTE: expected true or false, got: " ++ show b
   I.Deref _ty var expr
-    -> do val <- evalExpr expr
+    -> do val <- evalExpr _ty expr
           case val of
             Ref ref -> do
               val <- readStore ref
               writeStore (varSym var) val
             -- _ -> writeStore (varSym var) val
   I.Assign _ty var expr
-    -> do val <- evalExpr expr
+    -> do val <- evalExpr _ty expr
           writeStore (varSym var) val
   I.Local ty var init
     -> do val <- evalInit ty init
@@ -164,45 +210,84 @@ evalStmt stmt = case stmt of
   I.AllocRef _ty var ref
     -> writeStore (varSym var) (Ref $ nameSym ref)
   I.Loop var expr incr body
-    -> do val <- evalExpr expr
+    -> do val <- evalExpr I.ixRep expr
           writeStore (varSym var) val
           let (stepFn, doneExpr) = case incr of
-                I.IncrTo expr -> ((`add` Sint8 1), expr)
-                I.DecrTo expr -> ((`sub` Sint8 1), expr)
+                I.IncrTo expr -> ((`add` Sint32 1), expr) -- XXX: don't hard-code ixrep
+                I.DecrTo expr -> ((`sub` Sint32 1), expr)
           let step = modifyStore (varSym var) stepFn
           let done = do curVal  <- readStore (varSym var)
-                        doneVal <- evalExpr doneExpr
+                        doneVal <- evalExpr I.ixRep doneExpr
                         return (curVal == doneVal)
           untilM done (evalBlock body >> step)
   I.Return (I.Typed  _ty expr)
-    -> void $ evalExpr expr
+    -> void $ evalExpr _ty expr
   I.Store _ty (I.ExpVar dst) expr
-    -> do val <- evalExpr expr
+    -> do val <- evalExpr _ty expr
           Ref var <- readStore (varSym dst)
           writeStore var val
 
 evalAssert :: I.Expr -> Eval ()
 evalAssert asrt = do
-  b <- evalExpr asrt
+  b <- evalExpr I.TyBool asrt
   case b of
     Bool True -> return ()
     _         -> raise $ "Assertion failed: " ++ show (b,asrt)
 
-evalExpr :: I.Expr -> Eval Value
-evalExpr expr = case expr of
+evalExpr :: I.Type -> I.Expr -> Eval Value
+evalExpr ty expr = case expr of
   I.ExpSym sym -> readStore sym
   I.ExpVar var -> readStore (varSym var)
-  I.ExpLit lit -> evalLit lit
+  I.ExpLit lit -> evalLit ty lit
   I.ExpOp op exprs
-    -> do vals <- mapM evalExpr exprs
+    -> do let opTy = case op of
+                I.ExpEq t -> t
+                I.ExpNeq t -> t
+                I.ExpGt _ t -> t
+                I.ExpLt _ t -> t
+                I.ExpIsNan t -> t
+                I.ExpIsInf t -> t
+                _ -> ty
+          vals <- mapM (evalExpr opTy) exprs
           evalOp op vals
+  I.ExpSafeCast fromTy expr
+    -> do val <- evalExpr fromTy expr
+          return (cast ty fromTy val)
 
-evalLit :: I.Literal -> Eval Value
-evalLit lit = case lit of
-  I.LitInteger i -> return (Sint8 $ fromInteger i) -- XXX: fix
+cast :: I.Type -> I.Type -> Value -> Value
+cast toTy _fromTy val = mkVal toTy integer
+  where
+  integer = case val of
+    Sint8  i -> toInteger i
+    Sint16 i -> toInteger i
+    Sint32 i -> toInteger i
+    Sint64 i -> toInteger i
+    Uint8  i -> toInteger i
+    Uint16 i -> toInteger i
+    Uint32 i -> toInteger i
+    Uint64 i -> toInteger i
+
+evalLit :: I.Type -> I.Literal -> Eval Value
+evalLit ty lit = case lit of
+  I.LitInteger i -> return (mkVal ty i)
+  I.LitFloat c   -> return (Float c)
+  I.LitDouble c  -> return (Double c)
   I.LitChar c    -> return (Char c)
   I.LitBool b    -> return (Bool b)
   I.LitString s  -> return (String s)
+
+mkVal :: I.Type -> Integer -> Value
+mkVal ty = case ty of
+  I.TyInt  I.Int8   -> Sint8  . fromInteger
+  I.TyInt  I.Int16  -> Sint16 . fromInteger
+  I.TyInt  I.Int32  -> Sint32 . fromInteger
+  I.TyInt  I.Int64  -> Sint64 . fromInteger
+  I.TyWord I.Word8  -> Uint8  . fromInteger
+  I.TyWord I.Word16 -> Uint16 . fromInteger
+  I.TyWord I.Word32 -> Uint32 . fromInteger
+  I.TyWord I.Word64 -> Uint64 . fromInteger
+  I.TyIndex _       -> Sint32 . fromInteger -- XXX: don't hard-code index rep
+  _ -> error $ "mkVal: " ++ show ty
 
 evalOp :: I.ExpOp -> [Value] -> Eval Value
 evalOp (I.ExpEq _)       [x, y] = return (x `eq`  y)
@@ -226,14 +311,12 @@ evalOp I.ExpCond [cond, true, false] =
     Bool False -> return false
     _          -> raise $ "evalStmt: IfTE: expected true or false, got: " ++ show cond
 
-
-
 evalInit :: I.Type -> I.Init -> Eval Value
-evalInit _ty init = case init of
+evalInit ty init = case init of
   I.InitZero ->
-    return (Sint8 0)
+    return (mkVal ty 0)
   I.InitExpr _t expr ->
-    evalExpr expr
+    evalExpr _t expr
 
 varSym :: I.Var -> I.Sym
 varSym (I.VarName sym)     = sym
@@ -365,8 +448,7 @@ myloop =[ Local
                 (ExpOp
                    ExpAdd
                    [ ExpVar (VarName "deref3")
-                   , ExpVar (VarName "ix2")
-                   -- , ExpSafeCast (TyInt Int32) (ExpVar (VarName "ix2"))
+                   , ExpSafeCast (TyInt Int32) (ExpVar (VarName "ix2"))
                    ])
             ]
         , Deref
