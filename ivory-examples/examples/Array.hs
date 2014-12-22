6953ce616257db4eb3afdff6aa08b4273967f559
Pat Hickey <pat@galois.com>
2014-12-22 10:16:07 -0800

    ivory-examples: check in minimal test case for array cast codegen bug

--- ivory-examples/examples/Array.hs
+++ ivory-examples/examples/Array.hs
@@ -7,7 +7,9 @@ import Ivory.Language
 import Ivory.Compile.C.CmdlineFrontend
 
 cmodule :: Module
-cmodule = package "Array" $ incl arrayExample
+cmodule = package "Array" $ do
+  incl arrayExample
+  incl arrayTernary
 
 runArrayExample :: IO ()
 runArrayExample = runCompiler [cmodule] [] initialOpts { outDir = Nothing }
@@ -17,3 +19,14 @@ arrayExample = proc "arrayExample" $ \arr n -> body $ do
   arrayMap $ \ ix -> do
     v <- deref (arr ! ix)
     store (arr ! ix) (v + n)
+
+
+arrayTernary :: Def('[IBool] :-> IFloat)
+arrayTernary = proc "arrayTernary" $ \b -> body $ do
+  a1 <- local (vs 1)
+  a2 <- local (vs 2)
+  ares <- assign (b ? (a1, a2))
+  deref (ares ! 3) >>= ret
+  where
+  vs ::  IFloat -> Init (Array 4 (Stored IFloat))
+  vs v = iarray (map ival [v, v, v, v])
