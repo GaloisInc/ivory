13d7140fc32a724c0f8cdbcfb846cae893364e3c
leepike <leepike@gmail.com>
2015-01-24 23:00:27 -0800

    ivory-serialize: respect IvoryStore constraint for derefs.

--- ivory-serialize/src/Ivory/Serialize/Class.hs
+++ ivory-serialize/src/Ivory/Serialize/Class.hs
@@ -12,16 +12,26 @@ class SerializableRef t where
   -- Given a ref and a byte offset into an array, pack the value of t starting
   -- at the offset.
   packRef :: Ref s1 (CArray (Stored Uint8)) -> Uint32 -> ConstRef s2 t -> Ivory eff ()
-  default packRef :: (Serializable s, t ~ Stored s) => Ref s1 (CArray (Stored Uint8)) -> Uint32 -> ConstRef s2 t -> Ivory eff ()
+  default packRef :: (Serializable s, t ~ Stored s, IvoryStore s)
+                  => Ref s1 (CArray (Stored Uint8))
+                  -> Uint32
+                  -> ConstRef s2 t
+                  -> Ivory eff ()
   packRef into at from = deref from >>= pack into at
 
   -- Given a ref and a byte offset into an array, unpack the value of t starting
   -- at the offset.
-  unpackRef :: ConstRef s1 (CArray (Stored Uint8)) -> Uint32 -> Ref s2 t -> Ivory eff ()
-  default unpackRef :: (IvoryStore s, Serializable s, t ~ Stored s) => ConstRef s1 (CArray (Stored Uint8)) -> Uint32 -> Ref s2 t -> Ivory eff ()
+  unpackRef :: ConstRef s1 (CArray (Stored Uint8))
+            -> Uint32
+            -> Ref s2 t
+            -> Ivory eff ()
+  default unpackRef :: (IvoryStore s, Serializable s, t ~ Stored s)
+                    => ConstRef s1 (CArray (Stored Uint8))
+                    -> Uint32 -> Ref s2 t -> Ivory eff ()
   unpackRef from at to = unpack from at >>= store to
 
-instance (ANat len, IvoryArea a, SerializableRef a) => SerializableRef (Array len a) where
+instance (ANat len, IvoryArea a, SerializableRef a)
+      => SerializableRef (Array len a) where
   packRef dst offs src = arrayMap $ \ix ->
     packRef dst (offs + safeCast ix) (src ! ix)
 
