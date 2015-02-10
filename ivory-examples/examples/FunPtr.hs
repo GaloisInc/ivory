938ceb1a944e35c063354483d4efd78e3c1a6b35
leepike <leepike@gmail.com>
2015-02-09 17:27:22 -0800

    ivory-lang: fix up proc pointer example.

--- ivory-examples/examples/FunPtr.hs
+++ ivory-examples/examples/FunPtr.hs
@@ -9,8 +9,10 @@ import Ivory.Language
 f :: Def ('[Sint32] :-> Sint32)
 f  = proc "f" (\ n -> body (ret (n + 1)))
 
-invoke :: Def ('[ ProcPtr ('[Sint32] :-> Sint32), Sint32] :-> Sint32)
-invoke  = proc "invoke" (\ k n -> body (ret =<< indirect k n))
+invoke :: Def ('[ ProcPtr Global ('[Sint32] :-> Sint32), Sint32] :-> Sint32)
+invoke  = proc "invoke" $ \ k n -> body $ do
+  x <- indirect (3::Sint32) k n
+  ret x
 
 test :: Def ('[] :-> Sint32)
 test  = proc "test" (body (ret =<< call invoke (procPtr f) 10))
