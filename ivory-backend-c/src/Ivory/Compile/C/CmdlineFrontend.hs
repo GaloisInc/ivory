7ca45c44f966bf96eb04c58c48dc01ba272d42b0
leepike <leepike@gmail.com>
2015-03-12 15:43:22 -0700

    ivory-backend-c: check/warn if file overwritten.

--- ivory-backend-c/src/Ivory/Compile/C/CmdlineFrontend.hs
+++ ivory-backend-c/src/Ivory/Compile/C/CmdlineFrontend.hs
@@ -31,7 +31,7 @@ import qualified Ivory.Opts.TypeCheck       as T
 import Data.Maybe (mapMaybe, catMaybes)
 import Control.Monad (when)
 import Data.List (foldl')
-import System.Directory (createDirectoryIfMissing)
+import System.Directory (createDirectoryIfMissing, doesFileExist)
 import System.Environment (getArgs)
 import System.FilePath (addExtension,(</>))
 
@@ -194,7 +194,10 @@ rc sm modules artifacts opts
       out
       putStrLn " Done"
     where
-    out = writeFile fname contents
+    out = do
+      b <- doesFileExist fname
+      when b $ putStrLn ("*** Warning: overwriting " ++ fname)
+      writeFile fname contents
 
 --------------------------------------------------------------------------------
 
