389b3c50341d7011c2d696e4a01869e58885be8f
leepike <leepike@gmail.com>
2015-03-12 16:46:35 -0700

    ivory-backend-c: check if contents are equivalent before warning.

--- ivory-backend-c/src/Ivory/Compile/C/CmdlineFrontend.hs
+++ ivory-backend-c/src/Ivory/Compile/C/CmdlineFrontend.hs
@@ -196,7 +196,12 @@ rc sm modules artifacts opts
     where
     out = do
       b <- doesFileExist fname
-      when b $ putStrLn ("*** Warning: overwriting " ++ fname)
+      when b $ do
+        contents' <- readFile fname
+        -- XXX Should be a rare event. If files are large, can be changed to a
+        -- hash.
+        when (contents == contents')
+          (putStrLn ("*** Warning: overwriting " ++ fname))
       writeFile fname contents
 
 --------------------------------------------------------------------------------
