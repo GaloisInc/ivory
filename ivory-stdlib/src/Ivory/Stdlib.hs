aa9dfddcd3a003cdcebbe9eebba964cb51be3149
leepike <leepike@gmail.com>
2015-03-26 13:54:51 -0700

    ivory-stdlib: export Trig lib.

--- ivory-stdlib/src/Ivory/Stdlib.hs
+++ ivory-stdlib/src/Ivory/Stdlib.hs
@@ -1,20 +1,22 @@
 
 module Ivory.Stdlib
   ( module Ivory.Stdlib.Control
+  , module Ivory.Stdlib.Maybe
   , module Ivory.Stdlib.Memory
   , module Ivory.Stdlib.Operators
   , module Ivory.Stdlib.String
-  , module Ivory.Stdlib.Maybe
+  , module Ivory.Stdlib.Trig
   , stdlibModules
   ) where
 
 import Ivory.Language (Module)
 
 import Ivory.Stdlib.Control
+import Ivory.Stdlib.Maybe
 import Ivory.Stdlib.Memory
 import Ivory.Stdlib.Operators
 import Ivory.Stdlib.String
-import Ivory.Stdlib.Maybe
+import Ivory.Stdlib.Trig
 
 stdlibModules :: [Module]
 stdlibModules =
